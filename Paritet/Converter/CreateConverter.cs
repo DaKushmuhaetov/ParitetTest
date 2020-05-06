61f6107a86508d5e9f6de74c8d1699da470a01c8
DaKushmuhaetov <dank.9001@gmail.com>
2020-05-06 23:53:00 +0500

    Design features

--- Paritet/Converter/CreateConverter.cs
+++ Paritet/Converter/CreateConverter.cs
@@ -2,7 +2,7 @@
 using System.Globalization;
 using System.Windows.Data;
 
-namespace Paritet
+namespace Paritet.Converter
 {
     class CreateConverter : IMultiValueConverter
     {
---
+++ Paritet/Converter/DateTimeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Paritet.Converter
+{
+    public class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var year = ((DateTime)value).ToShortDateString();
+
+            return year;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
